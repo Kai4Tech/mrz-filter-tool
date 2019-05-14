a8bd763400471b0692c5a68e248524bcb31bc3ee
40502scott <ttocs321ttocs@yahoo.com.tw>
2019-05-14 11:10:43 -0400

    First commit

---
+++ Filter.py
@@ -0,0 +1,52 @@
+from PIL import Image, ImageEnhance
+
+def increase_contrast(img, level):  # 0~127
+    image = Image.open(img).convert("RGBA")
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+
+    def contrast(c):
+        value = 128 + factor * (c - 128)
+        return max(0, min(255, value))
+    new_frame = image.point(contrast)
+    path = frame_save(img, new_frame)
+    return path, new_frame
+
+
+def increase_contrast2(img, level):  # 0~3, 1 is original
+    image = Image.open(img).convert("RGBA")
+    factor = ImageEnhance.Contrast(image)
+    factor_im = factor.enhance(level)
+    path = frame_save(img, factor_im)
+    return path, factor_im
+
+
+def increase_brightness(img, level):  # 0~5 , 1 is original
+    image = Image.open(img).convert("RGBA")
+    factor = ImageEnhance.Brightness(image)
+    factor_im = factor.enhance(level)
+    path = frame_save(img, factor_im)
+    return path, factor_im
+
+
+def frame_save(name, factor):
+    if name.find('filtered') == 7:
+        path = name
+    else:
+        path = 'images/filtered/{}.png'.format(name[7:-4])
+    factor.save(path)
+    return path
+
+
+if __name__ == '__main__':
+    filename = 'images/passport_06.jpg'
+
+    frame = increase_brightness(filename, 3)
+
+    print(frame)
+
+    # frame = increase_contrast2(Image.open(file), 5)
+    #
+    # frame = increase_brightness(frame, 1)
+
+    # frame.save('images/filtered/{}2.png'.format(filename))
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='mrz-filter-tool',
+
+    scripts=['runner'],
+    version='1.0',
+    description='To increase the identify score for mrz. '
+                  'Change contrast or brightness to let score higher',
+    author='Kai4Tech',
+    author_email='ttocs321ttocs@yahoo.com.tw',
+    keywords='mrz-filter-tool'
+)
